abb48151da9fe0605a88bf7896c94f220d783993
hadioryani <Hadioryanipr@gmail.com>
2021-08-18 23:34:32 +0430

    ToDo List

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var input = document.getElementById("add");
+var btn = document.getElementById("btn");
+var list = document.getElementById("list");
+var el = document.getElementsByTagName("li");
+
+btn.addEventListener("click", () => {
+  var txt = input.value;
+  if (txt === "") {
+    alert("you must type something");
+  } else {
+    var li = document.createElement("li");
+    li.innerHTML = txt;
+    list.insertBefore(li, list.childNodes[0]);
+    input.value = "";
+  }
+});
+
+list.addEventListener("click", (e) => {
+  if (e.target.tagName == "LI") {
+    e.target.classList.toggle("done");
+  }
+});
